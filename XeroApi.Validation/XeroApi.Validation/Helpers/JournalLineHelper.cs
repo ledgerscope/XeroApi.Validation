85544e14770353daaf408d060db1eae83e63d37f
Matthew Steeples <matthew@mercuryit.co.uk>
2015-03-12 10:54:56 +0000

    Add helpers for journal lines

---
+++ XeroApi.Validation/XeroApi.Validation/Helpers/JournalLineHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XeroApi.Model;
+
+namespace XeroApi.Validation.Helpers
+{
+    public static class JournalLineHelper
+    {
+        public static decimal GetTotal(this JournalLine li)
+        {
+            return GetSubTotal(li) + li.TaxAmount;
+        }
+
+        public static decimal GetSubTotal(this JournalLine li)
+        {
+            return li.NetAmount;
+        }
+
+        public static decimal GetSubTotal(this IEnumerable<JournalLine> li)
+        {
+            return li.Sum(a => a.GetSubTotal());
+        }
+
+        public static decimal GetTotal(this IEnumerable<JournalLine> li)
+        {
+            return li.Sum(a => a.GetTotal());
+        }
+    }
+}
